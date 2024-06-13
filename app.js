457fb526576cc67f12e404640266a30dad8f33d2
unknown <shilpas2101@gmail.com>
2024-06-13 23:42:28 +0530

    view completed

--- app.js
+++ app.js
@@ -60,6 +60,26 @@ app.post("/login",(req,res)=>{
     )
     })
 
+
+app.post("/view",(req,res)=>{
+        let token=req.headers["token"]
+        jwt.verify(token,"user-app",(error,decoded)=>{
+            if (error) {
+                res.json({"status":"unauthorized access"})
+                
+            } else {
+                if (decoded) {
+                    usermodel.find().then(
+                        (response)=>{
+                            res.json(response)
+                        }
+                    ).catch()
+                    
+                }
+            }
+        })
+    })
+    
 app.listen(8080,()=>{
    console.log("server started")
 })
